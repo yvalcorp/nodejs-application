3bc4eb87316c49930bf4f5d0a7728d236a75d6d6
yvalcorp <yvalcorp@gmail.com>
2023-03-02 07:11:15 -0500

    sonarqube token added

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+  serverUrl: 'http://192.168.56.99:7000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.projectName': 'Node JS Application',
+	    'sonar.projectKey':'nodejskey',
+	    'sonar.login': '3sqa_901dd6485c97fa1592482a2703564e12aa7a3d4b',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
